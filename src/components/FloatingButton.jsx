190367301c9e39e83515de22a138e4825b4a4498
Aswin <aswinvsivan@gmail.com>
2025-05-20 16:44:11 +0200

    Update FloatingButton.jsx

--- src/components/FloatingButton.jsx
+++ src/components/FloatingButton.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import logo from '/logo.png'; // adjust the path if your logo is elsewhere
+import logo from '/logo.png'; 
 import '../styles/floatingButton.css'; 
 function FloatingButton() {
    const navigate = useNavigate();
